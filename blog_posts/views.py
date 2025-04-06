cbc2520ed8800a0b5af4e455d41d04f5ecc6d686
akadir <akadir.aweys@gmail.com>
2025-04-06 08:42:55 +0100

    first commit

---
+++ blog_posts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()  # Get all posts from the database
+    return render(request, "blog_posts/postlist.html", {"posts": posts})  # Corrected template path
